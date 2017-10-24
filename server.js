2950db99b2e50cd1d62fffb57dc98681829632ae
Calla Rudolph <callarudolph@gmail.com>
2017-10-24 11:32:26 -0700

    set up express

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+app.use(express.static('build'));
+
+var server = app.listen(process.env.PORT || 3000, function (){
+  var port = server.address().port;
+  console.log("app now running on port", port);
+});
 
 app.get('/', function (req, res) {
-  res.send('Hello Daylight!')
-})
+  res.sendFile(path.join(__dirname, 'build' + 'index.html'));
+});
+//template.ejs??
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
