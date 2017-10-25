7afe363c4854a9b6d41d1dfcdce9beb487c8370f
Calla Rudolph <callarudolph@gmail.com>
2017-10-25 10:42:29 -0700

    connect mongoDB locally through server

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,22 @@
 const express = require('express');
+var mongodb = require('mongodb');
+var Mongoose = require('mongoose');
+
+var TASKS_COLLECTION = 'tasks';
+
 const app = express();
 
 app.use(express.static(__dirname + './../../'));
+
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+
+Mongoose.connect(mongoDB);
+
+var db = Mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
 
 var server = app.listen(process.env.PORT || 3000, function (){
   var port = server.address().port;
