095db4037e4f088a4ccb33968adaba2154e537fb
Calla Rudolph <callarudolph@gmail.com>
2017-10-25 11:49:45 -0700

    connect to local mongo with mongoose

--- src/server/server.js
+++ src/server/server.js
@@ -1,21 +1,25 @@
-const express = require('express');
+var express = require('express');
 var mongodb = require('mongodb');
 var Mongoose = require('mongoose');
+const path = require('path');
 
-var TASKS_COLLECTION = 'tasks';
+var TASK_COLLECTION = 'tasks';
 
-const app = express();
+var app = express();
 
 app.use(express.static(__dirname + './../../'));
 
 var mongoDB = 'mongodb://127.0.0.1/my_database';
 
-Mongoose.connect(mongoDB);
+Mongoose.connect(mongoDB, {
+  useMongoClient: true
+});
 
 var db = Mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function callback() {
+  console.log("mongo connected");
 });
 
 var server = app.listen(process.env.PORT || 3000, function (){
@@ -28,3 +32,50 @@ function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
   res.status(code || 500).json({"error": message});
 }
+
+app.get('/api/tasks', function(req, res) {
+  db.collection(TASK_COLLECTION).find({}).sort({title: 1}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get tasks.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/tasks", function(req, res) {
+  var newTask = req.body;
+  newTask.createDate = new Date();
+
+  if (!req.body.title) {
+    handleError(res, "Invalid user input", "Must provide a title.", 400);
+  }
+
+  db.collection(TASK_COLLECTION).insertOne(newTask, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new task.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
+
+app.get("/api/tasks/:id", function(req, res) {
+  db.collection(TASK_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get task");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
+app.delete("/api/tasks/:id", function(req, res) {
+  db.collection(TASK_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete task");
+    } else {
+      res.status(200).json(req.params.id);
+    }
+  });
+});
