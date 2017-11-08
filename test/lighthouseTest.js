1ea38723594434527bccf2c69de869ad3a03fa01
Calla Rudolph <callarudolph@gmail.com>
2017-11-08 14:17:38 -0800

    pass test to check for proper parsing of lighthouse data

--- test/lighthouseTest.js
+++ test/lighthouseTest.js
@@ -8,6 +8,7 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../src/server/server');
 var should = chai.should();
+var expect = require('chai').expect;
 
 chai.use(chaiHttp);
 
@@ -15,9 +16,22 @@ chai.use(chaiHttp);
 describe('Lighthouse', () => {
   //test the /POST route
   describe('/POST lighthouse', () => {
-    it('it should return a lighthouse object', (done) => {
+    // it('it should return a lighthouse object', (done) => {
+    //   let url = {
+    //     url: "https://airhorner.com",
+    //   }
+    //   chai.request(server)
+    //     .post('/lighthouse')
+    //     .send(url)
+    //     .end((err, res) => {
+    //       res.should.have.status(200);
+    //       res.body.should.be.a('object');
+    //     done();
+    //   });
+    // });
+    it('it should contain relevant data through each path', (done) => {
       let url = {
-        url: "https://airhorner.com",
+        url: "https://maxobaxo.com",
       }
       chai.request(server)
         .post('/lighthouse')
@@ -25,9 +39,16 @@ describe('Lighthouse', () => {
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
+          expect(res.body.audits['first-meaningful-paint']).to.have.property('displayValue');
+          expect(res.body.audits['first-meaningful-paint']).to.have.property('score');
+          expect(res.body.audits['speed-index-metric']).to.have.property('score');
+          expect(res.body.reportCategories[1]).to.have.property('score');
+          expect(res.body.reportCategories[3]).to.have.property('score');
+          expect(res.body.reportCategories[2]).to.have.property('score');
         done();
       });
     });
   });
+
 
 });
