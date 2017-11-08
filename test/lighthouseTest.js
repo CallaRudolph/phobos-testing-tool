65e92666851e358f2ecf08fab3202fd1546cdb18
Calla Rudolph <callarudolph@gmail.com>
2017-11-08 14:56:08 -0800

    run tests through multiple urls

--- test/lighthouseTest.js
+++ test/lighthouseTest.js
@@ -29,13 +29,51 @@ describe('Lighthouse', () => {
     //     done();
     //   });
     // });
-    it('it should contain relevant data through each path', (done) => {
-      let url = {
+    it('it should parse out the data correctly in first link', (done) => {
+      let one = {
         url: "https://maxobaxo.com",
       }
       chai.request(server)
         .post('/lighthouse')
-        .send(url)
+        .send(one)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          expect(res.body.audits['first-meaningful-paint']).to.have.property('displayValue');
+          expect(res.body.audits['first-meaningful-paint']).to.have.property('score');
+          expect(res.body.audits['speed-index-metric']).to.have.property('score');
+          expect(res.body.reportCategories[1]).to.have.property('score');
+          expect(res.body.reportCategories[3]).to.have.property('score');
+          expect(res.body.reportCategories[2]).to.have.property('score');
+        done();
+      });
+    });
+    it('it should parse out the data correctly in second link', (done) => {
+      let two = {
+        url: "https://airhorner.com",
+      }
+      chai.request(server)
+        .post('/lighthouse')
+        .send(two)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          expect(res.body.audits['first-meaningful-paint']).to.have.property('displayValue');
+          expect(res.body.audits['first-meaningful-paint']).to.have.property('score');
+          expect(res.body.audits['speed-index-metric']).to.have.property('score');
+          expect(res.body.reportCategories[1]).to.have.property('score');
+          expect(res.body.reportCategories[3]).to.have.property('score');
+          expect(res.body.reportCategories[2]).to.have.property('score');
+        done();
+      });
+    });
+    it('it should parse out the data correctly in third link', (done) => {
+      let three = {
+        url: "https://argylewinery.com",
+      }
+      chai.request(server)
+        .post('/lighthouse')
+        .send(three)
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
@@ -49,6 +87,4 @@ describe('Lighthouse', () => {
       });
     });
   });
-
-
 });
