661d9f45e88f4897a315cfab678fedf0e01706ff
Calla Rudolph <callarudolph@gmail.com>
2017-11-09 14:28:00 -0800

    round scores to integers

--- src/client/components/Page.jsx
+++ src/client/components/Page.jsx
@@ -6,16 +6,16 @@ class Page extends Component {
   }
 
   render() {
-    let displayValue = this.props.data.audits['first-meaningful-paint']['displayValue'];
+    let firstPaint = this.props.data.audits['first-meaningful-paint']['displayValue'];
     let paintScore = this.props.data.audits['first-meaningful-paint']['score'];
     let speedScore = this.props.data.audits['speed-index-metric']['score'];
-    let performanceScore = this.props.data.reportCategories[1].score;
+    let performanceScore = (this.props.data.reportCategories[1].score).toFixed();
     let bestPracticeScore = this.props.data.reportCategories[3].score;
-    let accessibilityScore = this.props.data.reportCategories[2].score;
+    let accessibilityScore = (this.props.data.reportCategories[2].score).toFixed();
 
     return (
       <div>
-        <p>Display value: {displayValue}</p>
+        <p>First paint: {firstPaint}</p>
         <p>First meaningful paint score: {paintScore}</p>
         <p>Perceptual speed score: {speedScore}</p>
         <p>Performance score: {performanceScore}</p>
