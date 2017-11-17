dd43873e5b13f871de27a9d0e5e28d98689651b3
Calla Rudolph <callarudolph@gmail.com>
2017-11-16 16:29:51 -0800

    adjust test format

--- test/lighthouseTest.js
+++ test/lighthouseTest.js
@@ -48,43 +48,43 @@ describe('Lighthouse', () => {
         done();
       });
     });
-    // it('it should parse out the data correctly in second link', (done) => {
-    //   let two = {
-    //     url: "https://airhorner.com",
-    //   }
-    //   chai.request(server)
-    //     .post('/lighthouse')
-    //     .send(two)
-    //     .end((err, res) => {
-    //       res.should.have.status(200);
-    //       res.body.should.be.a('object');
-    //       expect(res.body.audits['first-meaningful-paint']).to.have.property('displayValue');
-    //       expect(res.body.audits['first-meaningful-paint']).to.have.property('score');
-    //       expect(res.body.audits['speed-index-metric']).to.have.property('score');
-    //       expect(res.body.reportCategories[1]).to.have.property('score');
-    //       expect(res.body.reportCategories[3]).to.have.property('score');
-    //       expect(res.body.reportCategories[2]).to.have.property('score');
-    //     done();
-    //   });
-    // });
-    // it('it should parse out the data correctly in third link', (done) => {
-    //   let three = {
-    //     url: "https://argylewinery.com",
-    //   }
-    //   chai.request(server)
-    //     .post('/lighthouse')
-    //     .send(three)
-    //     .end((err, res) => {
-    //       res.should.have.status(200);
-    //       res.body.should.be.a('object');
-    //       expect(res.body.audits['first-meaningful-paint']).to.have.property('displayValue');
-    //       expect(res.body.audits['first-meaningful-paint']).to.have.property('score');
-    //       expect(res.body.audits['speed-index-metric']).to.have.property('score');
-    //       expect(res.body.reportCategories[1]).to.have.property('score');
-    //       expect(res.body.reportCategories[3]).to.have.property('score');
-    //       expect(res.body.reportCategories[2]).to.have.property('score');
-    //     done();
-    //   });
+    it('it should parse out the data correctly in second link', (done) => {
+      let two = {
+        url: "https://airhorner.com",
+      }
+      chai.request(server)
+        .post('/lighthouse')
+        .send(two)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          expect(res.body.audits['first-meaningful-paint']).to.have.property('displayValue');
+          expect(res.body.audits['first-meaningful-paint']).to.have.property('score');
+          expect(res.body.audits['speed-index-metric']).to.have.property('score');
+          expect(res.body.reportCategories[1]).to.have.property('score');
+          expect(res.body.reportCategories[3]).to.have.property('score');
+          expect(res.body.reportCategories[2]).to.have.property('score');
+        done();
+      });
+    });
+    it('it should parse out the data correctly in third link', (done) => {
+      let three = {
+        url: "https://argylewinery.com",
+      }
+      chai.request(server)
+        .post('/lighthouse')
+        .send(three)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          expect(res.body.audits['first-meaningful-paint']).to.have.property('displayValue');
+          expect(res.body.audits['first-meaningful-paint']).to.have.property('score');
+          expect(res.body.audits['speed-index-metric']).to.have.property('score');
+          expect(res.body.reportCategories[1]).to.have.property('score');
+          expect(res.body.reportCategories[3]).to.have.property('score');
+          expect(res.body.reportCategories[2]).to.have.property('score');
+        done();
+      });
     });
   });
 });
