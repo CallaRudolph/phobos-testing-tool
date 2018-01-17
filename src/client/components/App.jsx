d1aaa941c3a6905527e2816754c88e98cdc1c404
Calla Rudolph <callarudolph@gmail.com>
2018-01-17 13:34:09 -0800

    expand performance opportunities for display

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import TaskInput from './TaskInput.jsx';
 import UrlCrawler from './UrlCrawler.jsx';
 import LighthouseDetail from './LighthouseDetail.jsx';
+import LighthouseSingle from './LHSingle.jsx';
 
 class App extends Component {
   render() {
@@ -11,9 +12,11 @@ class App extends Component {
         <p><em>A project to automate basic testing of websites using Google Lighthouse API.</em></p>
         <br/>
         {/* <TaskInput/> */}
-        <UrlCrawler/>
-        <br/>
+        {/* <UrlCrawler/>
+        <br/> */}
         <LighthouseDetail/>
+        <br/>
+        <LighthouseSingle/>
       </div>
     )
   }
