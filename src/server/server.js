73c60defb4a26523d4491ef018190e51a95c8bb5
Calla Rudolph <callarudolph@gmail.com>
2018-03-01 13:17:31 -0800

    remove server error when dropping mongoose database

--- src/server/server.js
+++ src/server/server.js
@@ -24,8 +24,10 @@ if(process.env.MONGODB_URI) {
 } else {
   var mongoDB = 'mongodb://127.0.0.1/my_database';
   mongoose.createConnection(mongoDB);
-  // mongoose.connect(mongoDB, function(){
-  //   /* Drop the DB if needed */
+
+  /* Drop the DB if needed::: */
+  // mongoose.connect(mongoDB, { useMongoClient: true, promiseLibrary: global.Promise }, function(){
+  //   // useMongoClient & promiseLibrary cancel out server error message: (node:2029) DeprecationWarning: `open()` is deprecated in mongoose >= 4.11.0, use `openUri()` instead, or set the `useMongoClient` option if using `connect()` or `createConnection()`. See http://mongoosejs.com/docs/connections.html#use-mongo-client
   //   mongoose.connection.db.dropDatabase();
   // });
 }
